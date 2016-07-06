1450577e8f65c3655f1bae61d3f676760067e508
John Best <jkbest@gmail.com>
2016-07-05 22:01:34 -0800

    Start module file, add .gitignore.

--- src/BoreholePaperPackage.jl
+++ src/BoreholePaperPackage.jl
@@ -1,5 +1,10 @@
 module BoreholePaperPackage
     using DataFrames, Distributions
+    using StatsFuns
     using HDF5, JLD
     using ProgressMeter
     using PyPlot
+
+    include("import_CCHRC.jl")
+    include("
+end
